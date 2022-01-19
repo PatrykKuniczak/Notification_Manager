bca0993b54a6c72ffbfd3084573fca8ec9a90ed7
Destrox <p.kuniczak@gmail.com>
2022-01-19 23:34:02 +0100

    reformat template

--- express_app/src/Server.ts
+++ express_app/src/Server.ts
@@ -1,29 +1,11 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
 import logger from "./shared/Logger";
-import bodyParser from "body-parser";
-import BaseRouter from "./controller";
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 
-(async () => {
-    try {
-        const conn = await createConnection();
-        logger.info('Connection to database: ' + conn.isConnected)
-    } catch (e: any) {
-        logger.err(e.message)
-    }
-})()
 
+createConnection().then(res => {
+    logger.info('Connection to database: ' + res.isConnected)
+}).catch(res => {
+    logger.err(res.message)
+})
 
-const app = express()
-
-app.use(express.json());
-app.use(bodyParser.json())
-app.use(cors())
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-app.use('/api', BaseRouter);
-
-export default app;
--- express_app/src/index.ts
+++ express_app/src/index.ts
@@ -1,5 +1,18 @@
+import "./Server"
 import logger from "./shared/Logger";
-import app from "./Server";
+import baseRouter from "./controller";
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use('/api', baseRouter);
 
 app.listen(9000, () => {
     logger.info('Express server started on port: 9000');
