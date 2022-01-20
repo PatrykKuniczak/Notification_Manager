bec325b6d811176ff6d3b66dfe2d81b75b548408
Destrox <p.kuniczak@gmail.com>
2022-01-20 22:41:41 +0100

    add morgan

--- express_app/src/index.ts
+++ express_app/src/index.ts
@@ -2,6 +2,7 @@ import "./Server"
 import logger from "./shared/Logger";
 import baseRouter from "./controller";
 import express from "express"
+import morgan from 'morgan';
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
@@ -12,8 +13,8 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(morgan("dev"));
 app.use('/api', baseRouter);
-
 app.listen(9000, () => {
     logger.info('Express server started on port: 9000');
 });
