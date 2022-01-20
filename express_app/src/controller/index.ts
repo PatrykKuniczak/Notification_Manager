34088860b5b9e2c5fdc3bd3361c7857472429b96
Destrox <p.kuniczak@gmail.com>
2022-01-20 22:44:22 +0100

    create notifications controller

--- express_app/src/controller/index.ts
+++ express_app/src/controller/index.ts
@@ -1,5 +1,20 @@
 import {Router} from "express";
+import {
+    addNotification,
+    deleteNotification,
+    displayAllNotifications,
+    displayNotification,
+    displayNotificationById,
+    editNotification
+} from "./notificationController";
 
-const baseRouter = Router();
+const baseRouter: Router = Router();
+
+baseRouter.post("/notification/create", addNotification)
+baseRouter.put("/notification/edit/:id", editNotification)
+baseRouter.delete("/notification/delete/:id", deleteNotification)
+baseRouter.get("/notification/all", displayAllNotifications)
+baseRouter.get("/notification/:id", displayNotificationById)
+baseRouter.get("/notification/:field/:data", displayNotification)
 
 export default baseRouter;
---
+++ express_app/src/controller/notificationController.ts
@@ -0,0 +1,105 @@
+import {Request, Response} from "express";
+import {Notification} from "../database/entity/Notification";
+import {DeleteResult, getManager, UpdateResult} from "typeorm";
+import {checkResult} from "./helpers";
+
+
+export async function addNotification(req: Request, res: Response): Promise<void> {
+    let {name, description} = req.body;
+    name = name.trim();
+    description = description.trim();
+
+    if (name && description !== "") {
+        try {
+            const result: Notification = await getManager().create(Notification, {name, description});
+            await getManager().save(result);
+            return res.sendStatus(201);
+
+        } catch (err: any) {
+            return res.sendStatus(404);
+        }
+    } else {
+        return res.sendStatus(400);
+    }
+}
+
+export async function editNotification(req: Request, res: Response): Promise<void> {
+    let {field, newData} = req.body;
+    field = field.trim();
+    newData = newData.trim();
+
+    if (newData && field !== "") {
+        try {
+            req.params.id = Number(req.params.id);
+            const result: UpdateResult = await getManager().update(Notification, req.params.id, {[field]: newData});
+            const status: number = checkResult(result);
+
+            return res.sendStatus(status);
+
+        } catch (err: any) {
+            return res.sendStatus(404);
+        }
+    } else {
+        return res.sendStatus(404);
+    }
+
+}
+
+export async function deleteNotification(req: Request, res: Response): Promise<void> {
+    if (req.params.id !== undefined) {
+        try {
+            req.params.id = Number(req.params.id);
+            const result: DeleteResult = await getManager().delete(Notification, req.params.id);
+            const status: number = checkResult(result);
+
+            return res.sendStatus(status);
+        } catch (err: any) {
+            return res.sendStatus(404);
+        }
+    } else {
+        return res.sendStatus(404);
+    }
+}
+
+export async function displayAllNotifications(req: Request, res: Response): Promise<Notification[]> {
+    const result: Notification[] = await getManager().find(Notification);
+    return res.send(result)
+}
+
+export async function displayNotification(req: Request, res: Response): Promise<void> {
+    const field = req.params.field.trim();
+    const data = req.params.data.trim();
+
+    if (field && data !== "") {
+        try {
+            const result: Notification | object = await getManager().findOne(Notification, {[field]: data});
+            if (result === undefined)
+                return res.sendStatus(404);
+
+            return res.send(result)
+        } catch (err: any) {
+            return res.sendStatus(404)
+        }
+    } else {
+        return res.sendStatus(404);
+    }
+
+}
+
+export async function displayNotificationById(req: Request, res: Response): Promise<void> {
+    const id = req.params.id.trim();
+
+    if (id !== "") {
+        try {
+            const result: Notification | object = await getManager().findOne(Notification, id);
+            if (result === undefined)
+                return res.sendStatus(404);
+
+            return res.send(result);
+        } catch (err: any) {
+            return res.sendStatus(404)
+        }
+    } else {
+        return res.sendStatus(404);
+    }
+}
