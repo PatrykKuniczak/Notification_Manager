508879f055762182cc7c54340ce9d2bca55157b0
Destrox <p.kuniczak@gmail.com>
2022-02-23 17:01:39 +0100

    create menu for mobile devices

--- react_app/src/components/sidebar/Menu.tsx
+++ react_app/src/components/sidebar/Menu.tsx
@@ -1,33 +1,63 @@
-import logo from "./img/logo.svg";
-import activeEye from "./img/activeEye.svg";
-import unActiveEye from "./img/unActiveEye.svg";
+import logo from "./icons/logo.svg";
+import activeEye from "./icons/activeEye.svg";
+import unActiveEye from "./icons/unActiveEye.svg";
 import {Link, NavLink} from 'react-router-dom'
 import styles from "./Menu.module.scss"
-import React from "react";
+import React, {useState} from "react";
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import {Button} from "react-bootstrap";
+import listIcon from "./icons/listIcon.svg";
 
 
-function Menu() {
+const ToggleMenu: React.FC<any> = (props) => {
+    const {show, handleClose} = props;
 
-    return (
-        <aside className={styles.menu}>
-            <Link to="/active"><img src={logo} alt="logo"/></Link>
+    return <Offcanvas className={styles["toggle-menu"]} show={show} onHide={handleClose} backdrop={true}>
+        <Offcanvas.Header bsPrefix={styles["toggle-menu-header"]} closeButton/>
+        <Offcanvas.Body bsPrefix={styles["toggle-menu-body"]}>
+            <Link className={styles.logo} to="/active"><img src={logo} alt="logo"/></Link>
             <ul>
                 <li>
                     <NavLink className={({isActive}) => (isActive && styles.active) || ''}
                              to="active">
-                        <div className={styles["iconSquare"]}/>
+                        <div className={styles["icon-square"]}/>
                         <img className={styles["icon"]} src={activeEye} alt="openEye"/>
                         <span>Aktywne</span></NavLink>
                 </li>
 
                 <li><NavLink className={({isActive}) => (isActive && styles.active) || ''}
-                             to="unActive">
-                    <div className={styles["iconSquare"]}/>
+                             to="inactive">
+                    <div className={styles["icon-square"]}/>
                     <img className={styles["icon"]} src={unActiveEye} alt="closeEye"/>
-                    <span>Zarchiwizowane</span></NavLink></li>
+                    <span>Zarchiwizowane</span></NavLink>
+                </li>
             </ul>
-        </aside>
-    )
+
+            <div className={styles['form-link-container']}>
+                <NavLink className={({isActive}) => (isActive && styles.active) || ''}
+                         to={"add-form"}>
+                    <button className={styles["add-button"]}>
+                        <b><i>Dodaj Zadanie</i></b>
+                    </button>
+                </NavLink>
+            </div>
+        </Offcanvas.Body>
+    </Offcanvas>
 }
+
+function Menu() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const toggleShow = () => setShow((s) => !s);
+
+    return <aside className={styles.menu}>
+
+        <Button onClick={toggleShow} className={styles["toggle-btn"]}><img src={listIcon} alt="List icon"/></Button>
+        <ToggleMenu show={show} handleClose={handleClose}/>
+
+    </aside>
+
+}
+
 
 export default Menu;
