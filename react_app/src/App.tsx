7e03f04ecb8e71638b198c8f51a29146bf2721cc
Destrox <p.kuniczak@gmail.com>
2022-03-05 19:25:09 +0100

    refactor items component and add loading spinner

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -2,23 +2,24 @@ import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import Home from './page/Home';
 import Items from "./components/mainContent/Items";
 import AddForm from "./components/mainContent/AddForm";
+import Axios from "axios";
 
-export const API_URL = "http://localhost:9000/api";
+Axios.defaults.baseURL = "http://localhost:9000/api";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}>
-                    <Route path="" element={<Navigate to={'active'}/>}/>
-                    <Route path="active" element={<Items active={true}/>}/>
-                    <Route path="inactive" element={<Items active={false}/>}/>
-                    <Route path="add-form" element={<AddForm/>}/>
-                </Route>
-                <Route path="*" element={<Navigate to="/"/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
-}
+    function App() {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}>
+                        <Route path="" element={<Navigate to={'active'}/>}/>
+                        <Route path="active" element={<Items active={true}/>}/>
+                        <Route path="inactive" element={<Items active={false}/>}/>
+                        <Route path="add-form" element={<AddForm/>}/>
+                    </Route>
+                    <Route path="*" element={<Navigate to="/"/>}/>
+                </Routes>
+            </BrowserRouter>
+        );
+    }
 
 export default App;
--- react_app/src/components/mainContent/Items.tsx
+++ react_app/src/components/mainContent/Items.tsx
@@ -4,8 +4,8 @@ import star from "./icons/star.svg";
 import deleteIcon from "./icons/delete.svg";
 import {useLocation} from "react-router-dom";
 import Axios from "axios";
-import {API_URL} from "../../App";
 import {Task} from "../helpers/interfaces/Task";
+import {CirclesWithBar} from "react-loader-spinner";
 
 
 const Table: React.FC = (props) => {
@@ -21,34 +21,39 @@ const Table: React.FC = (props) => {
 const Items: React.FC<{ active: boolean }> = (props) => {
     const location = useLocation();
     const [items, setItems] = useState<Task[]>([]);
+    const [loading, setLoading] = useState<Boolean>(true);
+    const [errorOccur, setErrorOccur] = useState<Boolean>(false);
 
 
-    const displayAll = useCallback(() => {
-        return Axios.get(API_URL + "/tasks/all").then(({data}) => setItems(data));
+    const fetchAll = useCallback(() => {
+        return Axios.get("/tasks/all").then(({data}) => {
+            setItems(data);
+            setLoading(false);
+        }).catch(() => setErrorOccur(true));
     }, []);
 
     const className = !props.active ? styles['main-content']
         : [styles['main-content'], styles['main-content--active']].join(' ');
 
     const importantHandler = async (id: number, item: { title: string, description: string, important: boolean }) => {
-        await Axios.put(API_URL + `/tasks/${id}`, {...item, important: !item.important});
-        return await displayAll();
+        await Axios.put(`/tasks/${id}`, {...item, important: !item.important});
+        return await fetchAll();
     }
 
     const deleteHandler = useCallback(async (id: number) => {
-        await Axios.delete(API_URL + `/tasks/${id}`);
-        return await displayAll();
-    }, [displayAll])
-
+        await Axios.delete(`/tasks/${id}`);
+        return await fetchAll();
+    }, [fetchAll])
 
     useEffect(() => {
-        displayAll().then(items => items)
-    }, [displayAll])
+        fetchAll().then()
+    }, [fetchAll]);
 
 
     return <div className={className}>
         <h1>{location.pathname === "/active" ? "Aktywne" : "Zarchiwizowane"}</h1>
-        <Table>
+
+        {!errorOccur ? !loading ? <Table>
             {items.map(({id, title, description, important}) =>
                 <tr key={id}>
                     <td className={styles.title}>{title}</td>
@@ -65,7 +70,12 @@ const Items: React.FC<{ active: boolean }> = (props) => {
                         </button>
                     </td>
                 </tr>)}
-        </Table>
+        </Table> : <CirclesWithBar
+            color="#2d74e0"
+            outerCircleColor="#2678e1"
+            innerCircleColor="#4987f3"
+            barColor="#75716c"
+        /> : <h3 className={styles["error-message"]}>"Wystąpił problem, podczas łączenia z serwerem."</h3>}
     </div>
 }
 
