88126321a88d585890964e3988994599d1f77afe
Destrox <p.kuniczak@gmail.com>
2022-04-03 20:52:42 +0200

    Create list filtering

--- react_app/src/components/Items/Items.tsx
+++ react_app/src/components/Items/Items.tsx
@@ -1,14 +1,17 @@
-import styles from "./Items.module.scss";
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import star from "./icons/star.svg";
 import deleteIcon from "./icons/delete.svg";
 import editIcon from "./icons/edit.svg";
+import filterIcon from "./icons/filter.svg";
 import {useLocation, useNavigate} from "react-router-dom";
 import Axios from "axios";
 import {ITask} from "../helpers/Interfaces";
 import ErrorLoadingProvider from "../ErrorLoadingProvider/ErrorLoadingProvider";
 import CheckEmptiness from "../CheckEmptiness/CheckEmptiness";
 import dateFormat from "dateformat";
+import {Dropdown} from "react-bootstrap";
+import styles from "./Items.module.scss";
+import {sort} from "fast-sort";
 
 
 const Table: React.FC = ({children}) => {
@@ -30,6 +33,7 @@ const Items: React.FC = () => {
     const [errorOccur, setErrorOccur] = useState<boolean>(false);
     const [errorMessage, setErrorMessage] = useState<string>("");
     const [reload, setReload] = useState<boolean>(false);
+    const [filter, setFilter] = useState<"A-Z" | "Z-A" | "LATEST-DATE" | "EARLIER-DATE">("A-Z");
 
 
     const eventHandler = (id: number, item?: ITask) => {
@@ -44,6 +48,18 @@ const Items: React.FC = () => {
         })
     }
 
+    const filterItems = useCallback((data: ITask[]) => {
+        switch (filter) {
+            case "A-Z":
+                return sort(data).asc(item => item.title)
+            case "Z-A":
+                return sort(data).desc(item => item.title)
+            case "EARLIER-DATE":
+                return sort(data).asc(item => item.notificationDate)
+            case "LATEST-DATE":
+                return sort(data).desc(item => item.notificationDate)
+        }
+    }, [filter])
 
     useEffect(() => {
         const filteredData: ITask[] = [];
@@ -57,18 +73,33 @@ const Items: React.FC = () => {
                 }
             })
 
-            setItems(filteredData);
+            setItems(filterItems(filteredData)!);
             setLoading(false);
         }).catch(() => setErrorOccur(true));
 
         return () => {
             setReload(false)
         }
-    }, [reload, location, checkLocation]);
+    }, [reload, location, checkLocation, filter, filterItems]);
 
 
     return <div className={styles['main-content']}>
-        <h1>{checkLocation ? "Aktywne" : "Zarchiwizowane"}</h1>
+        <div className={"d-flex justify-content-between mb-5"}>
+            <h1>{checkLocation ? "Aktywne" : "Zarchiwizowane"}</h1>
+
+            <Dropdown bsPrefix={"d-flex dropdown"}>
+                <Dropdown.Toggle variant={"none"} id="dropdown-basic">
+                    <img src={filterIcon} alt="Filtering button"/>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu variant={"dark"}>
+                    <Dropdown.Item onClick={() => setFilter("A-Z")}>A-Z</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFilter("Z-A")}>Z-A</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFilter("EARLIER-DATE")}>Najwcześniejsza Data</Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFilter("LATEST-DATE")}>Najpóźniejsza Data</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
 
         <ErrorLoadingProvider loading={loading} errorOccur={errorOccur} errorMessage={errorMessage}>
             {Boolean(items.length) ? <Table>
