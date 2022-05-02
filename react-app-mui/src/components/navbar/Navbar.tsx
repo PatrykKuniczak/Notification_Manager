753ecd1a04e27d3e15dde47ac3df980ac5b87b46
Destrox <p.kuniczak@gmail.com>
2022-05-02 18:52:06 +0200

    reformat navbar

--- react-app-mui/src/components/navbar/Navbar.tsx
+++ react-app-mui/src/components/navbar/Navbar.tsx
@@ -1,21 +1,50 @@
 import * as React from 'react';
-import NavbarList from "./NavbarList.styled";
 import NavbarItem from "./NavbarItem.styled";
 import activeEye from "../icons/activeEye.svg";
 import inactiveEye from "../icons/inactiveEye.svg";
 import addIcon from "../icons/addIcon.svg";
+import styled from "styled-components";
 
+
+const NavbarList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 40px;
+  list-style-type: none;
+
+  @media (max-width: 700px) {
+    flex-direction: row;
+    margin: 0;
+  }
+`
+
+const NavbarContainer = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 15px;
+  background: linear-gradient(to right top, #163C94, #1A47AF);
+
+  @media (max-width: 700px) {
+    flex-direction: row;
+  }
+`
 
 const Navbar = () => {
     return (
-        <NavbarList>
-            <div>
+        <NavbarContainer>
+            <NavbarList>
                 <NavbarItem src={activeEye} alt={"Active Tasks Button"}/>
                 <NavbarItem src={inactiveEye} alt={"Inactive Tasks Button"}/>
-            </div>
+            </NavbarList>
 
-            <NavbarItem src={addIcon} alt={"Add Tasks Button"}/>
-        </NavbarList>
+            <NavbarList>
+                <NavbarItem src={addIcon} alt={"Add Tasks Button"}/>
+            </NavbarList>
+        </NavbarContainer>
     );
 }
 
--- react-app-mui/src/components/navbar/NavbarItem.styled.tsx
+++ react-app-mui/src/components/navbar/NavbarItem.styled.tsx
@@ -1,19 +1,18 @@
 import styled from "styled-components";
 
 
-const NavbarButton = styled.button`
+const ListElement = styled.li`
   background: none;
   border: none;
-  padding: 0;
-  height: 24px;
+  height: 30px;
   cursor: pointer;
 `
 
 const NavbarItem = ({src, alt}: { src: string, alt: string }) => {
     return (
-        <NavbarButton>
+        <ListElement>
             <img src={src} alt={alt}/>
-        </NavbarButton>
+        </ListElement>
     )
 }
 
