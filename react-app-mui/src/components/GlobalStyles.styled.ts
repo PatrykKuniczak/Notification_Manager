80c9ec97b5a0cb3708b074f30aeda8279d5796a3
Destrox <p.kuniczak@gmail.com>
2022-05-02 18:50:53 +0200

    remove unused and change global styles

--- react-app-mui/src/components/GlobalStyles.styled.ts
+++ react-app-mui/src/components/GlobalStyles.styled.ts
@@ -2,22 +2,26 @@ import {createGlobalStyle} from "styled-components";
 
 
 const GlobalStyles = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    background: linear-gradient(to right top, #203E70, #10397B);
   }
-  
+
   a {
     color: #fff;
     text-decoration: none;
   }
-  
+
   code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
--- react-app-mui/src/pages/Home.tsx
+++ react-app-mui/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Navbar from "../components/navbar/Navbar";
 import {Outlet} from "react-router-dom";
 import HomeContainer from "./HomeContainer.styled";
 
+
 const Home = () => {
     return (<HomeContainer>
         <Navbar/>
