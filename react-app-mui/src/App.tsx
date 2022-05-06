abbc4de532d1fe9d7c10a404c041a202da0c7962
Destrox <p.kuniczak@gmail.com>
2022-05-06 18:45:01 +0200

    create home directory and move components

--- react-app-mui/src/App.tsx
+++ react-app-mui/src/App.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import Axios from "axios";
 import React from "react";
-import Home from './pages/Home';
+import Home from './pages/home/Home';
 import GlobalStyles from "./components/GlobalStyles.styled";
 import Items from "./components/items/Items";
 import {Provider} from "react-redux";
--- react-app-mui/src/pages/home/HomeContainer.styled.ts
+++ react-app-mui/src/pages/home/HomeContainer.styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {S_SIZE} from "../helpers/constants";
+import {S_SIZE} from "../../helpers/constants";
 
 
 const HomeContainer = styled.div`
