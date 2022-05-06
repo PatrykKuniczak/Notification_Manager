a90d8e19a7d0f9402d047d24d498e12757e84bd6
Destrox <p.kuniczak@gmail.com>
2022-05-06 16:43:54 +0200

    create logical component for Items

--- react-app-mui/src/components/items/Items.tsx
+++ react-app-mui/src/components/items/Items.tsx
@@ -2,34 +2,28 @@ import Item from "./Item";
 import filterIcon from "../icons/filter.svg";
 import {ItemsHeader} from "./styles/Item/ItemsHeader";
 import {ItemsContainer, ItemsList} from "./styles/Items/ItemsContainer";
-import {DisplayFilter, FilteringOption} from "./logical/FilterFunctions";
-import {FilterButton, FilterContainer, FilterContent, FilterOption} from "./styles/Items/FilterContainer";
+import {FilterButton, FilterContainer, FilterContent} from "./styles/Items/FilterContainer";
 import {useRef} from "react";
-import {useOnClickOutside} from "usehooks-ts";
+import {useSelector} from "react-redux";
+import {selectFilter} from "../store/store";
+import ItemsFunc from "./logical/Items/ItemsFunc";
 
-
-const options = ["A-Z", "Z-A", "Najwcześniejsza Data", "Najpóźniejsza Data"];
 
 const Items = ({active}: { active: boolean }) => {
-    const {show, display, closeDisplay} = DisplayFilter();
-    const {option, changeOption} = FilteringOption();
-
     const ref = useRef(null);
 
-    useOnClickOutside(ref, closeDisplay)
+    const {show} = useSelector(selectFilter);
+    const {displayOptions, toggleFilterContainer} = ItemsFunc(ref);
+
 
     return (<ItemsContainer>
             <ItemsHeader>
                 <h1>{active ? "Aktywne" : "Zarchiwizowane"}</h1>
                 <FilterContainer ref={ref}>
-                    <FilterButton type="image" onClick={display}
-                                  src={filterIcon} alt="Przycisk Filtrowania"/>
-
+                    <FilterButton type="image" src={filterIcon} alt="Przycisk Filtrowania"
+                                  onClick={toggleFilterContainer}/>
                     {show && <FilterContent>
-                        {options.map(item => <FilterOption key={item} onClick={() => {
-                            changeOption(item);
-                            closeDisplay();
-                        }} disabled={item === option}>{item}</FilterOption>)}
+                        {displayOptions()}
                     </FilterContent>}
                 </FilterContainer>
             </ItemsHeader>
---
+++ react-app-mui/src/components/items/logical/Items/ItemsFunc.tsx
@@ -0,0 +1,32 @@
+import {changeOption, closeFilterDropdown, selectFilter, toggleFilterDropdown} from "../../../store/store";
+import {useDispatch, useSelector} from "react-redux";
+import {FilterOption} from "../../styles/Items/FilterContainer";
+import {useOnClickOutside} from "usehooks-ts";
+import {RefObject} from "react";
+import {IOptions} from "../../../../helpers/interfaces";
+
+
+const options: IOptions[] = ["A-Z", "Z-A", "Najwcześniejsza Data", "Najpóźniejsza Data"];
+
+const ItemsFunc = (ref: RefObject<HTMLDivElement>) => {
+    const dispatch = useDispatch();
+    const {filterOption} = useSelector(selectFilter);
+
+    useOnClickOutside(ref, () => dispatch(closeFilterDropdown()));
+
+    const selectFilterOption = (item: IOptions) => {
+        dispatch(changeOption(item));
+    }
+
+    const displayOptions = () => options.map(item =>
+        <FilterOption key={item} onClick={() => selectFilterOption(item)} disabled={item === filterOption}>
+            {item}
+        </FilterOption>)
+
+    const toggleFilterContainer = () => dispatch(toggleFilterDropdown());
+
+    return {displayOptions, toggleFilterContainer, selectFilterOption}
+}
+
+
+export default ItemsFunc;
