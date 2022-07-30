c67c2c1a60dfeae72c9eb4cc0a143af4a9f8e17e
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-07-30 14:25:26 +0200

    create style for search bar

--- react_app/src/components/taskSearch/styles/TaskSearchContainer.ts
+++ react_app/src/components/taskSearch/styles/TaskSearchContainer.ts
@@ -5,6 +5,13 @@ const TaskSearchInput = styled.input`
   width: 250px;
   height: 40px;
   border-radius: 5px;
+  background-color: #e0dede;
+  padding-left: 10px;
+
+  &::placeholder {
+    color: #000000;
+    font-weight: bold;
+  }
 `
 
 
